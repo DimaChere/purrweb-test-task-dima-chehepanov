f280cb3a4cad5141e556799d9304410f62efb5ec
DimaChere <dmitriy.cherepanov02@mail.ru>
2023-06-15 15:39:47 +0600

    save

---
+++ src/frontTask/script.js
@@ -0,0 +1,7 @@
+function switchVisible() {
+  document.getElementsByClassName("menuScreen")[0].style.display = "flex";
+}
+
+function switchHiden() {
+  document.getElementsByClassName("menuScreen")[0].style.display = "none";
+}
