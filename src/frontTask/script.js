bdb199c7f396825d5d85ae08287c20560b1974e3
DimaChere <dmitriy.cherepanov02@mail.ru>
2023-06-17 16:46:45 +0600

    littleChanges
    курсор поинтер

--- src/frontTask/script.js
+++ src/frontTask/script.js
@@ -46,51 +46,31 @@ function hideCookies() {
     "none";
 }
 
-// let inp = document.querySelector("#phone");
+// var inp = document.getElementById("phone");
 
-// window.addEventListener("DOMContentLoaded", function () {
-//   [].forEach.call(document.getElementById("phone"), function (input) {
-//     var keyCode;
-//     function mask(event) {
-//       event.keyCode && (keyCode = event.keyCode);
-//       var pos = this.selectionStart;
-//       if (pos < 3) event.preventDefault();
-//       var matrix = "+7 (___) ___ ____",
-//         i = 0,
-//         def = matrix.replace(/\D/g, ""),
-//         val = this.value.replace(/\D/g, ""),
-//         new_value = matrix.replace(/[_\d]/g, function (a) {
-//           return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-//         });
-//       i = new_value.indexOf("_");
-//       if (i != -1) {
-//         i < 5 && (i = 3);
-//         new_value = new_value.slice(0, i);
-//       }
-//       var reg = matrix
-//         .substr(0, this.value.length)
-//         .replace(/_+/g, function (a) {
-//           return "\\d{1," + a.length + "}";
-//         })
-//         .replace(/[+()]/g, "\\$&");
-//       reg = new RegExp("^" + reg + "$");
-//       if (
-//         !reg.test(this.value) ||
-//         this.value.length < 5 ||
-//         (keyCode > 47 && keyCode < 58)
-//       )
-//         this.value = new_value;
-//       if (event.type == "blur" && this.value.length < 5) this.value = "";
-//     }
+// inp.onclick = function () {
+//   inp.value = "+";
+// };
 
-//     input.addEventListener("input", mask, false);
-//     input.addEventListener("focus", mask, false);
-//     input.addEventListener("blur", mask, false);
-//     input.addEventListener("keydown", mask, false);
-//   });
-// });
+// var old = 0;
 
-// inp.addEventListener("keypress", (e) => {
-//   // Отменяем ввод не цифр
-//   if (!/\d/.test(e.key)) e.preventDefault();
-// });
+// inp.onkeydown = function () {
+//   var curLen = inp.value.length;
+
+//   if (curLen < old) {
+//     old--;
+//     return;
+//   }
+
+//   if (curLen == 2) inp.value = inp.value + "(";
+
+//   if (curLen == 6) inp.value = inp.value + ")-";
+
+//   if (curLen == 11) inp.value = inp.value + "-";
+
+//   if (curLen == 14) inp.value = inp.value + "-";
+
+//   if (curLen > 16) inp.value = inp.value.substring(0, inp.value.length - 1);
+
+//   old++;
+// };
