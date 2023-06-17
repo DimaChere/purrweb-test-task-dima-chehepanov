40bde9b52c21187101fd5f469412426848ec29fe
DimaChere <dmitriy.cherepanov02@mail.ru>
2023-06-17 11:53:37 +0600

    gotoviy_proekt
    Есть недочеты

--- src/frontTask/script.js
+++ src/frontTask/script.js
@@ -1,3 +1,5 @@
+const { error } = require("jquery");
+
 function switchVisible_menu() {
   document.getElementsByClassName("menuScreen")[0].style.display = "flex";
 }
@@ -44,51 +46,51 @@ function hideCookies() {
     "none";
 }
 
-let inp = document.querySelector("#phone");
+// let inp = document.querySelector("#phone");
 
-window.addEventListener("DOMContentLoaded", function () {
-  [].forEach.call(document.querySelectorAll("#phone"), function (input) {
-    var keyCode;
-    function mask(event) {
-      event.keyCode && (keyCode = event.keyCode);
-      var pos = this.selectionStart;
-      if (pos < 3) event.preventDefault();
-      var matrix = "+7 (___) ___ ____",
-        i = 0,
-        def = matrix.replace(/\D/g, ""),
-        val = this.value.replace(/\D/g, ""),
-        new_value = matrix.replace(/[_\d]/g, function (a) {
-          return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-        });
-      i = new_value.indexOf("_");
-      if (i != -1) {
-        i < 5 && (i = 3);
-        new_value = new_value.slice(0, i);
-      }
-      var reg = matrix
-        .substr(0, this.value.length)
-        .replace(/_+/g, function (a) {
-          return "\\d{1," + a.length + "}";
-        })
-        .replace(/[+()]/g, "\\$&");
-      reg = new RegExp("^" + reg + "$");
-      if (
-        !reg.test(this.value) ||
-        this.value.length < 5 ||
-        (keyCode > 47 && keyCode < 58)
-      )
-        this.value = new_value;
-      if (event.type == "blur" && this.value.length < 5) this.value = "";
-    }
+// window.addEventListener("DOMContentLoaded", function () {
+//   [].forEach.call(document.getElementById("phone"), function (input) {
+//     var keyCode;
+//     function mask(event) {
+//       event.keyCode && (keyCode = event.keyCode);
+//       var pos = this.selectionStart;
+//       if (pos < 3) event.preventDefault();
+//       var matrix = "+7 (___) ___ ____",
+//         i = 0,
+//         def = matrix.replace(/\D/g, ""),
+//         val = this.value.replace(/\D/g, ""),
+//         new_value = matrix.replace(/[_\d]/g, function (a) {
+//           return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+//         });
+//       i = new_value.indexOf("_");
+//       if (i != -1) {
+//         i < 5 && (i = 3);
+//         new_value = new_value.slice(0, i);
+//       }
+//       var reg = matrix
+//         .substr(0, this.value.length)
+//         .replace(/_+/g, function (a) {
+//           return "\\d{1," + a.length + "}";
+//         })
+//         .replace(/[+()]/g, "\\$&");
+//       reg = new RegExp("^" + reg + "$");
+//       if (
+//         !reg.test(this.value) ||
+//         this.value.length < 5 ||
+//         (keyCode > 47 && keyCode < 58)
+//       )
+//         this.value = new_value;
+//       if (event.type == "blur" && this.value.length < 5) this.value = "";
+//     }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false);
-  });
-});
+//     input.addEventListener("input", mask, false);
+//     input.addEventListener("focus", mask, false);
+//     input.addEventListener("blur", mask, false);
+//     input.addEventListener("keydown", mask, false);
+//   });
+// });
 
-inp.addEventListener("keypress", (e) => {
-  // Отменяем ввод не цифр
-  if (!/\d/.test(e.key)) e.preventDefault();
-});
+// inp.addEventListener("keypress", (e) => {
+//   // Отменяем ввод не цифр
+//   if (!/\d/.test(e.key)) e.preventDefault();
+// });
--- src/sliderTask/slider.js
+++ src/sliderTask/slider.js
@@ -1,9 +1,110 @@
 let slideIndex = 1;
+let prevSlide;
+let isAnimating = false;
 
 let stylesControl = (control) => {
-  showSlide((slideIndex += control));
+  if (!isAnimating) {
+    isAnimating = true;
+    prevSlide = slideIndex;
+    slideIndex += control;
+    changeSlide_btn(prevSlide, -control);
+    setTimeout(() => {
+      isAnimating = false;
+    }, 700);
+  }
 };
 
+function dotControl(dotNumber) {
+  if (!isAnimating) {
+    isAnimating = true;
+    prevSlide = slideIndex;
+    slideIndex = dotNumber;
+    changeSlide(slideIndex);
+    changeSlide(prevSlide);
+    setTimeout(() => {
+      isAnimating = false;
+    }, 700);
+  }
+}
+
+function changeSlide(number) {
+  let slide = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (slideIndex > slide.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slide.length;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  dots[slideIndex - 1].className += " active";
+  let start = Date.now();
+
+  let right;
+  if (slideIndex > number) {
+    right = -1;
+  } else {
+    right = 1;
+  }
+
+  let timer = setInterval(() => {
+    let timePased = Date.now() - start;
+    slide[number - 1].style.left = right * (timePased / 3) + "%";
+
+    if (timePased > 400) {
+      slide[number - 1].style.display = "none";
+      slide[number - 1].style.left = "0";
+
+      slide[slideIndex - 1].style.display = "block";
+      slide[slideIndex - 1].style.left =
+        -1 * right * 100 + (right * (timePased - 400)) / 3 + "%";
+      if (timePased > 700) {
+        slide[slideIndex - 1].style.left = 0;
+        clearInterval(timer);
+      }
+    }
+  }, 20);
+}
+
+function changeSlide_btn(number, right) {
+  let slide = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (slideIndex > slide.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slide.length;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  dots[slideIndex - 1].className += " active";
+  let start = Date.now();
+
+  let timer = setInterval(() => {
+    let timePased = Date.now() - start;
+    slide[number - 1].style.left = right * (timePased / 3) + "%";
+
+    if (timePased > 400) {
+      slide[number - 1].style.display = "none";
+      slide[number - 1].style.left = "0";
+
+      slide[slideIndex - 1].style.display = "block";
+      slide[slideIndex - 1].style.left =
+        -1 * right * 100 + (right * (timePased - 400)) / 3 + "%";
+      if (timePased > 700) {
+        slide[slideIndex - 1].style.left = 0;
+        clearInterval(timer);
+      }
+    }
+  }, 20);
+}
 function showSlide(number) {
   let slide = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
@@ -21,11 +122,6 @@ function showSlide(number) {
   }
   dots[slideIndex - 1].className += " active";
   slide[slideIndex - 1].style.display = "block";
-}
-
-function dotControl(dotNumber) {
-  slideIndex = dotNumber;
-  showSlide(slideIndex);
 }
 
 showSlide(slideIndex);
